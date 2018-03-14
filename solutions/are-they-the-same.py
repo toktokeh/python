1022e0030e80f1863a419827c8f6e4354f0848ba
toktokeh <toktokeh@gmail.com>
2018-03-14 10:54:45 -0700

    Remove "try" statement

--- solutions/are-they-the-same.py
+++ solutions/are-they-the-same.py
@@ -3,8 +3,7 @@ import unittest
 
 
 def comp(array1, array2):
-    try:
-        return sorted([i ** 2 for i in array1]) == sorted(array2)
+    return sorted([i ** 2 for i in array1]) == sorted(array2)
 
 
 class Test(unittest.TestCase):
